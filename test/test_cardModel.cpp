6ec0388b5c34e84e8813dec6dba31cfc5ad96c0d
Austin (Austin) Ball <bala3720@36captain.cs.uleth.ca>
2021-11-05 12:39:58 -0600

    Single test for CardModel to get us started.

---
+++ test/test_cardModel.cpp
@@ -0,0 +1,33 @@
+/**
+   test_cardModel.cpp
+   Tests cardModel class
+   Written by Austin Ball
+   on 05/11/2021
+   CPSC3720
+*/
+
+#include "CardModel.h"
+#include "gtest/gtest.h"
+
+class CardModelTest : public testing::Test {
+protected:
+   CardModel* c;
+   void SetUp() override {
+      c = new CardModel(1);
+   }
+   void TearDown() {
+      delete c;
+   }
+};
+
+TEST_F(CardModelTest, test_setNumber) {
+   int testInt = 2;
+   c->setNumber(testInt);
+   ASSERT_TRUE(c->getNumber() == testInt);
+   ASSERT_FALSE(c->getNumber() != testInt);
+
+   // Verify inverse after changing test value
+   c->setNumber(testInt + 1);
+   ASSERT_FALSE(c->getNumber() == testInt);
+   ASSERT_TRUE(c->getNumber() != testInt);
+}
