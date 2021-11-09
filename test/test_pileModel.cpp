ddeddf981744d2cd2b63381581f75043cdaed80e
Gurdeep Singh <gurdeepmundi10@gmail.com>
2021-11-09 14:02:14 -0700

    added test for test_getsize

--- test/test_pileModel.cpp
+++ test/test_pileModel.cpp
@@ -7,19 +7,27 @@
 */
 
 #include <PileModel.h>
+#include <CardModel.h>
 #include <gtest/gtest.h>
 
 class PileModelTest: public ::testing::Test {
  public:
   void SetUp() {
     p = new PileModel();
+    c = new CardModel();
   }
   void TearDown() {
     delete p;
   }
   PileModel* p;
+  CardModel* c;
 };
 
-TEST_F (PileModelTest, test_Constructor) {
+TEST_F(PileModelTest, test_Constructor) {
   EXPECT_EQ(p->getSize(), 0);
+}
+
+TEST_F(PileModelTest, test_getsize) {
+  p->addCard(c);
+  EXPECT_EQ(p->getSize(), 1);
 }
