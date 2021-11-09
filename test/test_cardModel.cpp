48b28620c5f6fbb9367543817a2a01da90b1e3b9
Gurdeep Singh <gurdeepmundi10@gmail.com>
2021-11-09 11:57:56 -0700

    bugfix isSkipBo

--- test/test_cardModel.cpp
+++ test/test_cardModel.cpp
@@ -34,7 +34,7 @@ TEST_F(CardModelTest, test_setNumber) {
 
 TEST_F(CardModelTest, test_isSkipBo) {
   c->setNumber(0);
-  ASSERT_TRUE(c->isSkipBo(this));
+  ASSERT_TRUE(c->isSkipBo(c));
   c->setNumber(1);
-  ASSERT_FALSE(c->isSkipBo(this));
+  ASSERT_FALSE(c->isSkipBo(c));
 }
