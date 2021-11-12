07ca826efb0a334652ddfbd302f6a34c145adada
Austin (Austin) Ball <bala3720@36robin.cs.uleth.ca>
2021-11-11 19:01:16 -0700

    tested more of the methods in DeckModel class, there's a couple methods left, but I want a better understanding of how they work with the code before I write the tests.

--- test/test_deckModel.cpp
+++ test/test_deckModel.cpp
@@ -52,8 +52,53 @@ TEST_F(DeckModelTest, test_shuffleDeck) {
     ASSERT_FALSE(test == d->getDeck());
 }
 
+// tests for additional condition that the top card has actually been removed
+TEST_F(DeckModelTest, test_getTopCard) {
+    // preserve original top card
+    CardModel* origTopCard = d->getDeck()[0];
+    // grab return from method
+    CardModel* retTopCard = d->getTopCard();
+    // verify new top card on deck
+    CardModel* nextTopCard = d->getDeck()[0];
+
+    ASSERT_TRUE(origTopCard == retTopCard);
+    ASSERT_FALSE(retTopCard == nextTopCard);
+}
+
+// verifies difference in behavior between getTopCard and getFirstCard
+TEST_F(DeckModelTest, test_getterDifference) {
+    // grab return from getTopCard
+    CardModel* getTopCard = d->getTopCard();
+    // grab return from getFirstCard
+    CardModel* getFirstCard = d->getFirstCard();
+    // verify second method didn't change top card
+    CardModel* nextTopCard = d->getDeck()[0];
+
+    ASSERT_FALSE(getTopCard == getFirstCard);
+    ASSERT_TRUE(getFirstCard == nextTopCard);
+}
+
+// only tests for correctness of return
 TEST_F(DeckModelTest, test_getFirstCard) {
     CardModel* test = d->getDeck()[0];
 
     ASSERT_TRUE(test->getNumber() == d->getFirstCard()->getNumber());
 }
+
+TEST_F(DeckModelTest, test_removeCardsDup) {
+    int origDeckSize = d->getDeck().size();
+    d->removeCardsDup(9);
+    ASSERT_FALSE(origDeckSize == d->getDeck().size());
+}
+
+TEST_F(DeckModelTest, test_getsize) {
+    int origDeckSize = d->getDeck().size();
+    int retDeckSize = d->getsize();
+    ASSERT_TRUE(origDeckSize == retDeckSize);
+    d->removeCardsDup(9);
+    int newRetSize = d->getsize();
+    ASSERT_FALSE(retDeckSize == newRetSize);
+}
+
+//TEST_F()
+
