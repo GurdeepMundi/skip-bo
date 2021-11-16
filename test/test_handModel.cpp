7b86ac33f0b8ef8fa4ed5fc907d07118c8e3c3ce
Muhammad Saleh <msalehlm@hotmail.com>
2021-11-16 12:41:53 -0700

    test_getNumberOfCards add and fix

--- test/test_handModel.cpp
+++ test/test_handModel.cpp
@@ -42,7 +42,7 @@ TEST_F(HandModelTest, test_Constructor) {
 
 TEST_F(HandModelTest, test_GetHand) {
   ASSERT_TRUE(h->GetHand().size() == 0);
-  h->addCard(c);
+  h->addCard(c1);
   ASSERT_TRUE(h->GetHand().size() == 1);
   h->removeCard(0);
   ASSERT_TRUE(h->GetHand().size() == 0);
@@ -73,7 +73,19 @@ TEST_F(HandModelTest, test_removeCard) {
 }
 
 TEST_F(HandModelTest, test_getNumberOfCards) {
-
+  ASSERT_TRUE(h->getNumberOfCards() == 0);
+  h->addCard(c1);
+  ASSERT_TRUE(h->getNumberOfCards() == 1);
+  h->addCard(c2);
+  ASSERT_TRUE(h->getNumberOfCards() == 2);
+  h->addCard(c3);
+  ASSERT_TRUE(h->getNumberOfCards() == 3);
+  h->removeCard(1);
+  ASSERT_TRUE(h->getNumberOfCards() == 2);
+  h->removeCard(0);
+  ASSERT_TRUE(h->getNumberOfCards() == 1);
+  h->removeCard(0);
+  ASSERT_TRUE(h->getNumberOfCards() == 0);
 }
 
 TEST_F(HandModelTest, test_useCard) {
