44622c235c3c8c15100ff055ad3b60f3bd0e8bff
Muhammad Saleh <msalehlm@hotmail.com>
2021-11-16 12:35:59 -0700

    test_GetHand fix

--- test/test_handModel.cpp
+++ test/test_handModel.cpp
@@ -36,7 +36,7 @@ TEST_F(HandModelTest, test_GetHand) {
   ASSERT_TRUE(h->GetHand().size() == 0);
   h->addCard(c);
   ASSERT_TRUE(h->GetHand().size() == 1);
-  h->removeCard(1);
+  h->removeCard(0);
   ASSERT_TRUE(h->GetHand().size() == 0);
 }
 
@@ -51,7 +51,17 @@ TEST_F(HandModelTest, test_addCard) {
 }
 
 TEST_F(HandModelTest, test_removeCard) {
-
+  ASSERT_TRUE(h->getNumberOfCards() == 0);
+  h->addCard(c);
+  h->addCard(c);
+  h->addCard(c);
+  ASSERT_TRUE(h->getNumberOfCards() == 3);
+  h->removeCard(3);
+  ASSERT_TRUE(h->getNumberOfCards() == 2);
+  h->removeCard(2);
+  ASSERT_TRUE(h->getNumberOfCards() == 1);
+  h->removeCard(1);
+  ASSERT_TRUE(h->getNumberOfCards() == 0);
 }
 
 TEST_F(HandModelTest, test_getNumberOfCards) {
