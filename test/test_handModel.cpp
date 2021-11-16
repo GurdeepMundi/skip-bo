3145099f171c072fda9b0fd8f923257190e7f790
Muhammad Saleh <msalehlm@hotmail.com>
2021-11-16 12:39:42 -0700

    test_removeCard Fix

--- test/test_handModel.cpp
+++ test/test_handModel.cpp
@@ -14,16 +14,24 @@
 class HandModelTest : public testing::Test {
  protected:
     HandModel* h;
-    CardModel* c;
+    CardModel* c1;
+    CardModel* c2;
+    CardModel* c3;
     void SetUp() override {
         h = new HandModel();
-        c = new CardModel(1);
+        c1 = new CardModel(1);
+        c2 = new CardModel(2);
+        c3 = new CardModel(3);
     }
     void TearDown() override {
         delete h;
-        delete c;
+        delete c1;
+        delete c2;
+        delete c3;
         h = NULL;
-        c = NULL;
+        c1 = NULL;
+        c2 = NULL;
+        c3 = NULL;
     }
 };
 
@@ -42,25 +50,25 @@ TEST_F(HandModelTest, test_GetHand) {
 
 TEST_F(HandModelTest, test_addCard) {
   ASSERT_TRUE(h->getNumberOfCards() == 0);
-  h->addCard(c);
+  h->addCard(c1);
   ASSERT_TRUE(h->getNumberOfCards() == 1);
-  h->addCard(c);
+  h->addCard(c2);
   ASSERT_TRUE(h->getNumberOfCards() == 2);
-  h->addCard(c);
+  h->addCard(c3);
   ASSERT_TRUE(h->getNumberOfCards() == 3);
 }
 
 TEST_F(HandModelTest, test_removeCard) {
   ASSERT_TRUE(h->getNumberOfCards() == 0);
-  h->addCard(c);
-  h->addCard(c);
-  h->addCard(c);
+  h->addCard(c1);
+  h->addCard(c2);
+  h->addCard(c3);
   ASSERT_TRUE(h->getNumberOfCards() == 3);
-  h->removeCard(3);
-  ASSERT_TRUE(h->getNumberOfCards() == 2);
   h->removeCard(2);
-  ASSERT_TRUE(h->getNumberOfCards() == 1);
+  ASSERT_TRUE(h->getNumberOfCards() == 2);
   h->removeCard(1);
+  ASSERT_TRUE(h->getNumberOfCards() == 1);
+  h->removeCard(0);
   ASSERT_TRUE(h->getNumberOfCards() == 0);
 }
 
