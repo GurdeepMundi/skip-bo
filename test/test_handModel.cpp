517f05d54e5eb066ea715ea62f76dcb03c00eea0
Muhammad Saleh <msalehlm@hotmail.com>
2021-11-17 10:30:52 -0700

    changed ASSERT_TRUE to EXPECT_EQ

--- test/test_handModel.cpp
+++ test/test_handModel.cpp
@@ -36,70 +36,70 @@ class HandModelTest : public testing::Test {
 };
 
 TEST_F(HandModelTest, test_Constructor) {
-  ASSERT_TRUE(h->getNumberOfCards() == 0);
-  ASSERT_TRUE(h->GetHand().size() == 0);
+  EXPECT_EQ(h->getNumberOfCards(), 0);
+  EXPECT_EQ(h->GetHand().size(), 0);
 }
 
 TEST_F(HandModelTest, test_GetHand) {
-  ASSERT_TRUE(h->GetHand().size() == 0);
+  EXPECT_EQ(h->GetHand().size(), 0);
   h->addCard(c1);
-  ASSERT_TRUE(h->GetHand().size() == 1);
+  EXPECT_EQ(h->GetHand().size(), 1);
   h->removeCard(0);
-  ASSERT_TRUE(h->GetHand().size() == 0);
+  EXPECT_EQ(h->GetHand().size(), 0);
 }
 
 TEST_F(HandModelTest, test_addCard) {
-  ASSERT_TRUE(h->getNumberOfCards() == 0);
+  EXPECT_EQ(h->getNumberOfCards(), 0);
   h->addCard(c1);
-  ASSERT_TRUE(h->getNumberOfCards() == 1);
+  EXPECT_EQ(h->getNumberOfCards(), 1);
   h->addCard(c2);
-  ASSERT_TRUE(h->getNumberOfCards() == 2);
+  EXPECT_EQ(h->getNumberOfCards(), 2);
   h->addCard(c3);
-  ASSERT_TRUE(h->getNumberOfCards() == 3);
+  EXPECT_EQ(h->getNumberOfCards(), 3);
   h->removeCard(0);
   h->removeCard(0);
   h->removeCard(0);
-  ASSERT_TRUE(h->getNumberOfCards() == 0);
+  EXPECT_EQ(h->getNumberOfCards(), 0);
 }
 
 TEST_F(HandModelTest, test_removeCard) {
-  ASSERT_TRUE(h->getNumberOfCards() == 0);
+  EXPECT_EQ(h->getNumberOfCards(), 0);
   h->addCard(c1);
   h->addCard(c2);
   h->addCard(c3);
-  ASSERT_TRUE(h->getNumberOfCards() == 3);
+  EXPECT_EQ(h->getNumberOfCards(), 3);
   h->removeCard(2);
-  ASSERT_TRUE(h->getNumberOfCards() == 2);
+  EXPECT_EQ(h->getNumberOfCards(), 2);
   h->removeCard(1);
-  ASSERT_TRUE(h->getNumberOfCards() == 1);
+  EXPECT_EQ(h->getNumberOfCards(), 1);
   h->removeCard(0);
-  ASSERT_TRUE(h->getNumberOfCards() == 0);
+  EXPECT_EQ(h->getNumberOfCards(), 0);
 }
 
 TEST_F(HandModelTest, test_getNumberOfCards) {
-  ASSERT_TRUE(h->getNumberOfCards() == 0);
+  EXPECT_EQ(h->getNumberOfCards(), 0);
   h->addCard(c1);
-  ASSERT_TRUE(h->getNumberOfCards() == 1);
+  EXPECT_EQ(h->getNumberOfCards(), 1);
   h->addCard(c2);
-  ASSERT_TRUE(h->getNumberOfCards() == 2);
+  EXPECT_EQ(h->getNumberOfCards(), 2);
   h->addCard(c3);
-  ASSERT_TRUE(h->getNumberOfCards() == 3);
+  EXPECT_EQ(h->getNumberOfCards(), 3);
   h->removeCard(1);
-  ASSERT_TRUE(h->getNumberOfCards() == 2);
+  EXPECT_EQ(h->getNumberOfCards(), 2);
   h->removeCard(0);
-  ASSERT_TRUE(h->getNumberOfCards() == 1);
+  EXPECT_EQ(h->getNumberOfCards(), 1);
   h->removeCard(0);
-  ASSERT_TRUE(h->getNumberOfCards() == 0);
+  EXPECT_EQ(h->getNumberOfCards(), 0);
 }
 
 TEST_F(HandModelTest, test_useCard) {
-  ASSERT_TRUE(h->getNumberOfCards() == 0);
+  EXPECT_EQ(h->getNumberOfCards(), 0);
   h->addCard(c1);
   h->addCard(c2);
   h->addCard(c3);
-  ASSERT_TRUE(h->useCard(0) == c1);
-  ASSERT_TRUE(h->useCard(1) == c2);
-  ASSERT_TRUE(h->useCard(2) == c3);
+  EXPECT_EQ(h->useCard(0), c1);
+  EXPECT_EQ(h->useCard(1), c2);
+  EXPECT_EQ(h->useCard(2), c3);
   h->removeCard(0);
   h->removeCard(0);
   h->removeCard(0);
