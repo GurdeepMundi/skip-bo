d97913212bcd0b5e9af33d79379880e94bc8c1eb
Gurdeep Singh <g.null@uleth.ca>
2021-11-22 13:22:30 -0700

    bugfix + created file for arrPileModelTest

---
+++ test/test_arrPileModel.cpp
@@ -0,0 +1,19 @@
+/**
+   test_pileModel.cpp
+   Tests PileModel class
+   Written by Gurdeep Singh
+   on 09/11/2021
+   CPSC3720
+*/
+#include <gtest/gtest.h>
+
+
+class arrPileModelTest: public testing::Test {
+ protected:
+  void SetUp() override {
+    //ctr
+  }
+  void TearDown() {
+    //dtr
+  }
+};
