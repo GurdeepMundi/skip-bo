52605b9ebb95a9f862e3bcaff0455c973c513360
Gurdeep Singh <gurdeepmundi10@gmail.com>
2021-11-22 23:21:15 -0700

    stylefixes

--- include/MockDeckModel.h
+++ include/MockDeckModel.h
@@ -22,18 +22,18 @@ using std::stack;
 
 class MockDeckModel : public DeckModel {
  public:
-   explicit MockDeckModel() {}
-   virtual ~MockDeckModel() {}
-    MOCK_METHOD0(shuffleDeck, void());
-    MOCK_METHOD0(getTopCard, CardModel*());
-    MOCK_METHOD0(buildDeck, std::vector<CardModel*>());
-    MOCK_METHOD0(getDeck, std::vector<CardModel*>());
-    MOCK_METHOD0(getGarbage, std::vector<CardModel*>());
-    MOCK_METHOD1(removeCardsDup, void(int));
-    MOCK_METHOD0(getsize, int());
-    MOCK_METHOD0(getFirstCard, CardModel*());
-    MOCK_METHOD0(addGarbageToDeck, void());
-    MOCK_METHOD0(checkSize, void());
-    MOCK_METHOD1(addBuildToGarbage, void(std::stack<CardModel*>));
+  explicit MockDeckModel() {}
+  virtual ~MockDeckModel() {}
+  MOCK_METHOD0(shuffleDeck, void());
+  MOCK_METHOD0(getTopCard, CardModel*());
+  MOCK_METHOD0(buildDeck, std::vector<CardModel*>());
+  MOCK_METHOD0(getDeck, std::vector<CardModel*>());
+  MOCK_METHOD0(getGarbage, std::vector<CardModel*>());
+  MOCK_METHOD1(removeCardsDup, void(int));
+  MOCK_METHOD0(getsize, int());
+  MOCK_METHOD0(getFirstCard, CardModel*());
+  MOCK_METHOD0(addGarbageToDeck, void());
+  MOCK_METHOD0(checkSize, void());
+  MOCK_METHOD1(addBuildToGarbage, void(std::stack<CardModel*>));
 };
 #endif
