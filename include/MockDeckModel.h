4e21df908a55948a7174f3ccb0d627427ad0e0e3
Gurdeep Singh <gurdeepmundi10@gmail.com>
2021-11-22 21:54:36 -0700

    commented more stuff

--- include/MockDeckModel.h
+++ include/MockDeckModel.h
@@ -19,7 +19,7 @@ using std::stack;
 class MockDeckModel : public DeckModel {
  public:
     MOCK_METHOD0(shuffleDeck, void());
-    MOCK_METHOD0(getTopCard, CardModel*());
+/*    MOCK_METHOD0(getTopCard, CardModel*());
     MOCK_METHOD0(buildDeck, std::vector<CardModel*>());
     MOCK_METHOD0(getDeck, std::vector<CardModel*>());
     MOCK_METHOD0(getGarbage, std::vector<CardModel*>());
@@ -28,6 +28,6 @@ class MockDeckModel : public DeckModel {
     MOCK_METHOD0(getFirstCard, CardModel*());
     MOCK_METHOD0(addGarbageToDeck, void());
     MOCK_METHOD0(checkSize, void());
-    MOCK_METHOD0(addBuildToGarbage, void(std::stack<CardModel*>));
+    MOCK_METHOD0(addBuildToGarbage, void(std::stack<CardModel*>));*/
 };
 #endif
